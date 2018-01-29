2788c2ff3d5186c34028857bb81b572cd59afee8
Valentin Trifonov <valioplayofficial@gmail.com>
2018-01-29 22:54:29 +0200

    Intro and Basic Syntax Lab

---
+++ Intro and Basic Syntax Lab/02. Add Two Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        var a = int.Parse(Console.ReadLine());
+        var b = int.Parse(Console.ReadLine());
+        var sum = a + b;
+
+        Console.WriteLine("{0} + {1} = {2}", a, b, sum);
+    }
+}
---
+++ Intro and Basic Syntax Lab/03. Employee Data/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        var name = Console.ReadLine();
+        var age = int.Parse(Console.ReadLine());
+        var id = int.Parse(Console.ReadLine());
+        var salary = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Name: {0}", name);
+        Console.WriteLine("Age: {0}", age);
+        Console.WriteLine("Employee ID: {0}", id.ToString("D8"));
+        Console.WriteLine("Salary: {0:F2}", salary);
+    }
+}
