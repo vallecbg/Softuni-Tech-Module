c42046641e90c72ce1339c9b92dc8e76256e6cf2
Valentin Trifonov <valentintrifonov03@gmail.com>
2018-05-01 14:33:10 +0300

    adding solution

---
+++ Software Technologies/JAVASCRIPT EXPRESSJS OVERVIEW - EXERCISES/expressjs-lab/models/Calculator.js
@@ -0,0 +1,37 @@
+function Calculator (leftOperand, operator, rightOperand){
+    this.leftOperand = leftOperand;
+    this.operator = operator;
+    this.rightOperand = rightOperand;
+
+    this.calculateResult = function() {
+        let result = 0;
+        switch (this.operator) {
+            case "+":
+                result = this.leftOperand + this.rightOperand;
+                break;
+            case "-":
+                result = this.leftOperand - this.rightOperand;
+                break;
+            case "*":
+                result = this.leftOperand * this.rightOperand;
+                break;
+            case "/":
+                result = this.leftOperand / this.rightOperand;
+                break;
+            case "^":
+                result = Math.pow(this.leftOperand, this.rightOperand);
+                break;
+            case "%":
+                result = this.leftOperand % this.rightOperand;
+                break;
+        }
+        if(isFinite(result) === false){
+            result = "Cannot divide by 0";
+        }
+
+
+        return result;
+    }
+}
+
+module.exports = Calculator;
