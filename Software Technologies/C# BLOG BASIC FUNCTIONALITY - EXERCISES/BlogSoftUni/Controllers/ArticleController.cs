8781ba7d410327920c17f6afef1015a366770f9f
Valentin Trifonov <valentintrifonov03@gmail.com>
2018-05-01 16:10:44 +0300

    adding solution

---
+++ Software Technologies/C# BLOG BASIC FUNCTIONALITY - EXERCISES/BlogSoftUni/Controllers/ArticleController.cs
@@ -0,0 +1,213 @@
+﻿using BlogSoftUni.Data;
+using BlogSoftUni.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BlogSoftUni.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArticleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        
+
+        //
+        // GET: Article
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        //
+        // GET: Article/List
+        public ActionResult List()
+        {
+            var articles = _context.Articles
+                .Include(a => a.Author)
+                .ToList();
+
+            return View(articles);
+        }
+
+        //
+        // GET: Article/Details
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return StatusCode(500);
+            }
+
+            var article = _context.Articles
+                .Include(a => a.Author)
+                .First(m => m.Id == id);
+
+            if (article == null)
+            {
+                return StatusCode(500);
+            }
+
+            return View(article);
+        }
+
+        //
+        // GET: Article/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: Article/Create
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                //get user id
+                var authorId = _context.Users
+                    .Where(u => u.UserName == this.User.Identity.Name)
+                    .First()
+                    .Id;
+                //set articles author
+                article.AuthorId = authorId;
+                //save article
+                _context.Articles.Add(article);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(article);
+        }
+
+
+        //
+        // GET: Article/Delete
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var article = _context.Articles
+                .Include(a => a.Author)
+                .First(m => m.Id == id);
+
+            if (IsUserAuthorizedToEdit(article) == false)
+            {
+                return Forbid();
+            }
+
+            if (article == null)
+            {
+                return StatusCode(500);
+            }
+
+            return View(article);
+        }
+
+        //
+        // Post: Article/Delete/id
+        [HttpPost, ActionName("Delete")]
+        [Authorize]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            //get article
+            var article = _context.Articles
+                .Include(a => a.Author)
+                .First(m => m.Id == id);
+
+
+
+            //check if article exists
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            //delete article
+            _context.Articles.Remove(article);
+            _context.SaveChanges();
+            //redirect to index
+            return RedirectToAction("Index");
+        }
+
+        //
+        // GET: Article/Edit/Id
+        [Authorize]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var article = _context.Articles
+                .Include(a => a.Author)
+                .First(m => m.Id == id);
+
+            if (IsUserAuthorizedToEdit(article) == false)
+            {
+                return Forbid();
+            }
+
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            var model = new ArticleViewModel();
+            model.Id = article.Id;
+            model.Title = article.Title;
+            model.Content = article.Content;
+
+            return View(model);
+        }
+        
+        //
+        // POST: Article/Edit/id
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, ArticleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var article = _context.Articles
+                    .Where(a => a.Id == model.Id)
+                    .Include(a => a.Author)
+                    .FirstOrDefault();
+
+                article.Title = model.Title;
+                article.Content = model.Content;
+
+                _context.Update(article);
+                _context.SaveChanges();
+
+                return RedirectToAction($"Details/{article.Id}");
+            }
+
+            return View(model);
+        }
+
+        private bool IsUserAuthorizedToEdit(Article article)
+        {
+            bool isAuthor = article.IsAuthor(User.Identity.Name);
+            bool isAdmin = User.IsInRole("Admin");
+
+            return isAdmin || isAuthor;
+        }
+    }
+}
